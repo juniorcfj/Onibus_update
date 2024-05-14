b86a29c5d54b2d773b306b59a5e8a19636e57e2c
itamar junior <91996555+juniorcfj@users.noreply.github.com>
2024-05-14 16:07:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,19 @@
                 <option value="a-b">De A para B (R$10,00)</option>
                 <option value="a-c">De A para C (R$20,00)</option>
             </select>
+             <div class="info-section">
+            <h2>Informações do Motorista</h2>
+            <p>Nome: <?php echo $motorista->nome; ?></p>
+            <p>CNH: <?php echo $motorista->cnh; ?></p>
+
+            <h2>Informações do Ônibus</h2>
+            <p>Modelo: <?php echo $onibus->modelo; ?></p>
+            <p>Placa: <?php echo $onibus->placa; ?></p>
+
+            <h2>Informações das Viagens</h2>
+            <p>Origem: <?php echo $viagemAB->origem; ?> - Destino: <?php echo $viagemAB->destino; ?> - Valor: R$<?php echo $viagemAB->valor; ?></p>
+            <p>Origem: <?php echo $viagemAC->origem; ?> - Destino: <?php echo $viagemAC->destino; ?> - Valor: R$<?php echo $viagemAC->valor; ?></p>
+        </div>
             <button type="submit" id="comprarButton">Comprar Passagem</button>
         </form>
     </div>
